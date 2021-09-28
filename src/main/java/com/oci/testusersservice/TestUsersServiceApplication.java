6b3d15815806025eadf63845c42be47e48b60ef7
Karlos Chita <karloslcp@gmail.com>
2021-09-28 15:11:55 -0500

    Initial commit

---
+++ src/main/java/com/oci/testusersservice/TestUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oci.testusersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUsersServiceApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TestUsersServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/oci/testusersservice/TestUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oci.testusersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUsersServiceApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+}
